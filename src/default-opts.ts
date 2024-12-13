cb665140bb9a6ed5cb0cfe937bba5e9748c75000
Sergey Krainov <itsib.su@gmail.com>
2024-12-13 14:22:20 +0700

    Fixed default settings

--- src/default-opts.ts
+++ src/default-opts.ts
@@ -1,8 +1,8 @@
 import { AppConfig } from './types/app-config.ts';
 
 export const DEFAULT_CONFIG: Omit<AppConfig, 'input' | 'output'> = {
-  name: 'IconFont1',
-  prefix: 'icon1',
+  name: 'IconFont',
+  prefix: 'icon',
   types: [ 'woff2', 'woff', 'ttf', 'eot'],
   port: 9001,
   fontUrl: './',
