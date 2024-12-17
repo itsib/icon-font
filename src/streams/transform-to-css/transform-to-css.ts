c5015fba9711972fba4e546b5ac9c50998341cf0
Sergey Krainov <itsib.su@gmail.com>
2024-12-17 16:48:01 +0700

    Finished detail modal and animation selector

--- src/streams/transform-to-css/transform-to-css.ts
+++ src/streams/transform-to-css/transform-to-css.ts
@@ -6,7 +6,7 @@ import { join } from 'node:path';
 import { encodeCss, encodeHtml } from '../../utils/coders.ts';
 
 const STYLES = `
-.icon {
+.{{prefix}} {
   font-family: "{{fontName}}", sans-serif;
   font-weight: 500;
   font-size: 1em;
@@ -20,72 +20,72 @@ const STYLES = `
   -webkit-font-smoothing: antialiased;
 }
 
-.icon-1x {
+.{{prefix}}-1x {
   font-size: 1em
 }
-.icon-2x {
+.{{prefix}}-2x {
   font-size: 2em
 }
-.icon-3x {
+.{{prefix}}-3x {
   font-size: 3em
 }
-.icon-4x {
+.{{prefix}}-4x {
   font-size: 4em
 }
-.icon-5x {
+.{{prefix}}-5x {
   font-size: 5em
 }
-.icon-6x {
+.{{prefix}}-6x {
   font-size: 6em
 }
-.icon-7x {
+.{{prefix}}-7x {
   font-size: 7em
 }
-.icon-8x {
+.{{prefix}}-8x {
   font-size: 8em
 }
-.icon-9x {
+.{{prefix}}-9x {
   font-size: 9em
 }
-.icon-10x {
+.{{prefix}}-10x {
   font-size: 10em
 }
-.icon-2xs {
+.{{prefix}}-2xs {
   font-size: .625em;
   line-height: .1em;
   vertical-align: .225em
 }
-.icon-xs {
+.{{prefix}}-xs {
   font-size: .75em;
   line-height: .08333em;
   vertical-align: .125em
 }
-.icon-sm {
+.{{prefix}}-sm {
   font-size: .875em;
   line-height: .07143em;
   vertical-align: .05357em
 }
-.icon-lg {
+.{{prefix}}-lg {
   font-size: 1.25em;
   line-height: .05em;
   vertical-align: -.075em
 }
-.icon-xl {
+.{{prefix}}-xl {
   font-size: 1.5em;
   line-height: .04167em;
   vertical-align: -.125em
 }
-.icon-2xl {
+.{{prefix}}-2xl {
   font-size: 2em;
   line-height: .03125em;
   vertical-align: -.1875em
 }
-.icon-fw {
+.{{prefix}}-fw {
   text-align: center;
   width: 1.25em
 }
 
-.icon-beat {
+.{{prefix}}-beat {
   animation-name: icon-motion-beat;
   animation-delay: var(--icon-animation-delay, 0s);
   animation-direction: var(--icon-animation-direction, normal);
@@ -94,7 +94,7 @@ const STYLES = `
   animation-timing-function: var(--icon-animation-timing, ease-in-out)
 }
 
-.icon-bounce {
+.{{prefix}}-bounce {
   animation-name: icon-motion-bounce;
   animation-delay: var(--icon-animation-delay, 0s);
   animation-direction: var(--icon-animation-direction, normal);
@@ -103,26 +103,26 @@ const STYLES = `
   animation-timing-function: var(--icon-animation-timing, cubic-bezier(.28,.84,.42,1));
 }
 
-.icon-fade {
+.{{prefix}}-fade {
   animation-name: icon-motion-fade;
   animation-iteration-count: var(--icon-animation-iteration-count, infinite);
   animation-timing-function: var(--icon-animation-timing, cubic-bezier(.4,0,.6,1));
 }
 
-.icon-beat-fade,
-.icon-fade {
+.{{prefix}}-beat-fade,
+.{{prefix}}-fade {
   animation-delay: var(--icon-animation-delay,0s);
   animation-direction: var(--icon-animation-direction,normal);
   animation-duration: var(--icon-animation-duration,1s);
 }
 
-.icon-beat-fade {
+.{{prefix}}-beat-fade {
   animation-name: icon-motion-beat-fade;
   animation-iteration-count: var(--icon-animation-iteration-count,infinite);
   animation-timing-function: var(--icon-animation-timing,cubic-bezier(.4,0,.6,1));
 }
 
-.icon-flip {
+.{{prefix}}-flip {
   animation-name: icon-motion-flip;
   animation-delay: var(--icon-animation-delay,0s);
   animation-direction: var(--icon-animation-direction,normal);
@@ -131,32 +131,35 @@ const STYLES = `
   animation-timing-function: var(--icon-animation-timing,ease-in-out);
 }
 
-.icon-shake {
+.{{prefix}}-shake {
   animation-name: icon-motion-shake;
   animation-duration: var(--icon-animation-duration,1s);
   animation-iteration-count: var(--icon-animation-iteration-count,infinite);
   animation-timing-function: var(--icon-animation-timing,linear);
 }
 
-.icon-shake,
-.icon-spin {
+.{{prefix}}-spin-reverse {
+  --icon-animation-direction: reverse;
+}
+
+.{{prefix}}-shake,
+.{{prefix}}-spin-reverse,
+.{{prefix}}-spin {
   animation-delay: var(--icon-animation-delay,0s);
   animation-direction: var(--icon-animation-direction,normal);
 }
 
-.icon-spin {
+.{{prefix}}-spin-reverse,
+.{{prefix}}-spin {
   animation-name: icon-motion-spin;
   animation-duration: var(--icon-animation-duration,2s);
   animation-iteration-count: var(--icon-animation-iteration-count,infinite);
   animation-timing-function: var(--icon-animation-timing,linear);
 }
 
-.icon-spin-reverse {
-  --icon-animation-direction: reverse;
-}
 
-.icon-pulse,
-.icon-spin-pulse {
+.{{prefix}}-pulse,
+.{{prefix}}-spin-pulse {
   animation-name: icon-motion-spin;
   animation-direction: var(--icon-animation-direction, normal);
   animation-duration: var(--icon-animation-duration, 1s);
@@ -165,7 +168,16 @@ const STYLES = `
 }
 
 @media (prefers-reduced-motion:reduce) {
-  .icon-beat, .icon-beat-fade, .icon-bounce, .icon-fade, .icon-flip, .icon-pulse, .icon-shake, .icon-spin, .icon-spin-pulse {
+  .{{prefix}}-beat, 
+  .{{prefix}}-beat-fade, 
+  .{{prefix}}-bounce, 
+  .{{prefix}}-fade, 
+  .{{prefix}}-flip, 
+  .{{prefix}}-pulse, 
+  .{{prefix}}-shake, 
+  .{{prefix}}-spin-reverse, 
+  .{{prefix}}-spin, 
+  .{{prefix}}-spin-pulse {
     animation-delay: -1ms;
     animation-duration: 1ms;
     animation-iteration-count: 1;
@@ -342,7 +354,7 @@ export class TransformToCss extends Transform {
     }
     output += '}\n';
 
-    output += STYLES.replace('{{fontName}}', this._fontName) + '\n';
+    output += STYLES.replace('{{fontName}}', this._fontName).replace(/\{\{prefix}}/g, this._prefix) + '\n';
 
     return output;
   }
--- src/streams/transform-to-html/transform-to-html.ts
+++ src/streams/transform-to-html/transform-to-html.ts
@@ -10,14 +10,17 @@ const HEAD = `
   <style>
     html {
       --border-radius: 8px;
-      --preview-bg: rgba(2, 6, 23, 0.7);
-      --label-bg: #0c4a6e;
+      --preview-bg: 2 6 23;
+      --label-bg: 12 74 110;
 
       color: #e2e8f0;
       background: #0f172a;
     }
     body {
-      font-family: "Ubuntu Sans", Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif, ui-sans-serif, 
+                    system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", 
+                    Roboto, "Noto Sans", "Apple Color Emoji", "Segoe UI Emoji", 
+                   "Segoe UI Symbol", "Noto Color Emoji";
       font-weight: 300;
       font-size: 16px;
       margin: 0;
@@ -32,7 +35,6 @@ const HEAD = `
     .caption svg {
       width: auto;
       height: 56px;
-      
     }
     .caption svg .text-paths {
       color: white !important;
@@ -60,7 +62,7 @@ const HEAD = `
       display: inline-block;
       width: 100%;
       text-align: center;
-      background: var(--preview-bg);
+      background: rgb(var(--preview-bg) / 0.7);
       border-radius: var(--border-radius) var(--border-radius) 0 0;
     }
     .preview .inner i {
@@ -72,12 +74,13 @@ const HEAD = `
       width: 100%;
       box-sizing: border-box;
       padding: 5px;
-      font-size: 11px;
+      font-size: 12px;
+      font-weight: 500;
       font-family: Monaco, monospace;
       white-space: nowrap;
       overflow: hidden;
       text-overflow: ellipsis;
-      background: var(--label-bg);
+      background: rgb(var(--label-bg));
       border-radius: 0 0 var(--border-radius) var(--border-radius);
     }
     [aria-label] {
@@ -132,13 +135,261 @@ const HEAD = `
       opacity: 1;
       transform: translate(-50%, 4px);
     }
+
+    dialog {
+      width: 500px;
+      padding: 0;
+      color: white;
+      border: 1px solid rgba(255 255 255 / 0.1);
+      background-color: rgb(var(--preview-bg) / 0.9);
+      -webkit-backdrop-filter: saturate(180%) blur(20px);
+      backdrop-filter: saturate(180%) blur(20px);
+      box-shadow: 0 0 2px 1px rgb(0 0 0 / .1);
+      border-radius: var(--border-radius);
+      animation: fade-out 0.2s ease-out;
+      z-index: 5000;
+      overflow: visible;
+      user-select: none;
+      -moz-user-select: none;
+      -webkit-user-select: none;
+    }
+    dialog[open] {
+      animation: fade-in 0.2s ease-out;
+    }
+    dialog[open]::backdrop {
+      animation: backdrop-fade-in 0.2s ease-out forwards;
+    }
+    dialog .header {
+      margin: 0;
+      padding: 10px 15px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    dialog .header .close {
+      left: 10px;
+      top: -3px;
+      width: 30px;
+      height: 30px;
+      font-size: 20px;
+      color: white;
+      background: transparent;
+      border: none;
+      border-radius: 50%;
+      position: relative;
+      cursor: pointer;
+    }
+    dialog .content {
+      padding: 0 15px 10px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+    dialog .content .cell-1 {
+      width: 160px;
+      height: 80px;
+      padding: 40px 0;
+    }
+    dialog .content .cell-1 .icon {
+      display: block;
+    }
+    dialog .content .cell-2 {
+      width: calc(100% - 180px);
+      position: relative;
+    }
+    dialog .content .cell-2 .copy {
+      right: 8px;
+      top: 8px;
+      padding: 0;
+      color: rgb(255 255 255 / 0.7);
+      background: transparent;
+      border: none;
+      position: absolute;
+    }
+    dialog .content .cell-2 .copy:hover {
+      color: rgb(255 255 255 / 1);
+    }
+    dialog .content .cell-2 .example {
+      min-height: 80px;
+      padding: 32px 20px 18px;
+      border-radius: 6px;
+      border: 1px solid rgb(255 255 255 / 0.2);
+      background: rgb(10 10 10);
+      overflow-x: auto;
+      box-sizing: border-box;
+    }
+    dialog .content .cell-2 code {
+      font-size: 13px;
+      white-space: nowrap;
+       user-select: text;
+      -moz-user-select: text;
+      -webkit-user-select: text;
+    }
+    dialog .content .cell-3 {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+    }
+    dialog .content .cell-3 .caption {
+      margin-bottom: 8px;
+      font-size: 14px;
+      font-weight: 500;
+      text-align: start;
+    }
+    dialog .content .cell-3 .buttons {
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      column-gap: 4px;
+    }
+    dialog .content .cell-3 .buttons button {
+      height: 22px;
+      margin-bottom: 4px;
+      padding: 2px 6px;
+      font-size: 12px;
+      line-height: 1;
+      color: white;
+      background-color: rgba(255 255 255 / 0.1);
+      border: none;
+      border-radius: 3px;
+      display: inline-block;
+      cursor: pointer;
+    }
+    dialog .content .cell-3 .buttons button.active {
+      background-color: rgb(var(--label-bg));
+    }
+    
+    dialog ::-webkit-scrollbar {
+      width: 4px;
+      height: 4px;
+      transition: all .15s ease-in-out;
+    }
+    dialog ::-webkit-scrollbar-track {
+      padding: 4px 0;
+      position: relative;
+      background-color: transparent;
+    }
+    dialog ::-webkit-scrollbar-button {
+      height: 8px;
+      background-color: transparent;
+    }
+    dialog ::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      background-color: rgb(255 255 255 / 0.5);
+    }
+    dialog ::-webkit-scrollbar-corner {
+      display: none;
+    }
+
+    @keyframes fade-in {
+      0% {
+        opacity: 0;
+        transform: translateY(-20px);
+        display: none;
+      }
+
+      100% {
+        opacity: 1;
+        transform: translateY(0);
+        display: block;
+      }
+    }
+    @keyframes fade-out {
+      0% {
+        opacity: 1;
+        transform: translateY(0);
+        display: block;
+      }
+
+      100% {
+        opacity: 0;
+        transform: translateY(-20px);
+        display: none;
+      }
+    }
+    @keyframes backdrop-fade-in {
+      0% {
+        background-color: rgb(0 0 0 / 0%);
+      }
+
+      100% {
+        background-color: rgb(0 0 0 / 25%);
+      }
+    }
   </style>
 
   <link rel="stylesheet" type="text/css" href="/style.css"/>
 
   <script type="application/javascript">
-    function onClickCallback(button) {
-      navigator.clipboard.writeText(button.dataset.text).then(() => {
+    let activeAnimationButton; 
+    let prefix;
+    
+    const dialog = document.getElementById('icon-dialog');
+    
+    dialog.addEventListener('cancel', () => {
+      console.log('cancel');
+    })
+    
+    function renderExample() {
+      const iconDemo = document.getElementById('icon-demo');
+      const code = document.getElementById('code-example');
+      const className = iconDemo.className.replace(' icon-5x', '');
+      
+      code.dataset.source = '<i class="' + className + '"></i>';
+      let html = '<span style="color: #D5B778">&#60;i </span>';
+      html += '<span style="color: #BABABA">class=</span>';
+      html += '<span style="color: #6AAB73">&#34;' + className + '&#34;</span>';
+      html += '<span style="color: #D5B778">&#62;&#60;/i&#62;</span>';
+      
+      code.innerHTML = html;
+    }
+    function onChooseIcon(button) {
+      
+      const header = document.getElementById('dialog-header');
+      const iconDemo = document.getElementById('icon-demo');
+
+      prefix = button.dataset.prefix;
+      header.innerText = button.dataset.name;
+      iconDemo.className = button.dataset.prefix + ' ' + button.dataset.class + ' icon-5x';
+
+      renderExample();
+      
+      dialog.showModal();
+    }
+    function onClose() {
+      dialog.close();
+     
+      if (activeAnimationButton) {
+        activeAnimationButton.classList.remove('active');
+        activeAnimationButton = undefined;
+      }
+    }
+    function onAnimationClick(button) {
+      const iconDemo = document.getElementById('icon-demo');
+      
+      const toDisable = button.classList.contains('active');
+      
+      if (activeAnimationButton) {
+        activeAnimationButton.classList.remove('active');
+        iconDemo.classList.remove(prefix + '-' + activeAnimationButton.dataset.animation);
+        activeAnimationButton = undefined;
+      }
+      
+      if (toDisable) {
+        renderExample();
+        return;
+      }
+      
+      button.classList.add('active')
+      iconDemo.classList.add(prefix + '-' + button.dataset.animation);
+      activeAnimationButton = button;
+      
+      renderExample();
+    }
+    function onCopy(button) {
+      const code = document.getElementById('code-example');
+      navigator.clipboard.writeText(code.dataset.source).then(() => {
         const label = button.getAttribute('aria-label');
         button.setAttribute('aria-label', 'Copied ✔');
 
@@ -192,6 +443,47 @@ const HEAD = `
 </head>
 `;
 
+const DIALOG = `
+<dialog id="icon-dialog" role="alertdialog" aria-modal="true">
+  <h3 class="header">
+    <div id="dialog-header">Dialog</div>
+
+    <button onclick="onClose()" type="button" class="close">✕</button>
+  </h3>
+  
+  <div class="content">
+    <div class="cell-1" >
+      <i id="icon-demo" class="icon icon-folder-key icon-5x"></i>
+    </div>
+    <div class="cell-2">
+      <div class="example">
+        <code id="code-example"></code>
+      </div>
+      <button type="button" class="copy" aria-label="Copy to clipboard" onclick="onCopy(this)">
+        <svg viewBox="0 0 512 512" width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+          <path fill="currentColor" d="m81.454 16c-35.902 0-65.454 29.552-65.454 65.454v218.18c0 35.902 29.552 65.454 65.454 65.454a21.818 21.818 0 0 0 21.818-21.818 21.818 21.818 0 0 0-21.818-21.818c-12.099 0-21.818-9.719-21.818-21.818v-218.18c0-12.099 9.719-21.818 21.818-21.818h218.18c12.099 0 21.818 9.719 21.818 21.818a21.818 21.818 0 0 0 21.818 21.818 21.818 21.818 0 0 0 21.818-21.818c0-35.902-29.552-65.454-65.454-65.454zm130.91 130.91c-35.884 0-65.454 29.57-65.454 65.454v218.18c0 35.884 29.57 65.454 65.454 65.454h218.18c35.884 0 65.454-29.57 65.454-65.454v-218.18c0-35.884-29.57-65.454-65.454-65.454zm0 43.636h218.18c12.465 0 21.818 9.3534 21.818 21.818v218.18c0 12.465-9.3534 21.818-21.818 21.818h-218.18c-12.465 0-21.818-9.3534-21.818-21.818v-218.18c0-12.465 9.3534-21.818 21.818-21.818z" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </button>
+    </div>
+    <div class="cell-3">
+      <div class="caption">Animations</div>
+      <div id="animation-buttons" class="buttons">
+        <button type="button" data-animation="beat" onclick="onAnimationClick(this)">beat</button>
+        <button type="button" data-animation="bounce" onclick="onAnimationClick(this)">bounce</button>
+        <button type="button" data-animation="fade" onclick="onAnimationClick(this)">fade</button>
+        <button type="button" data-animation="beat-fade" onclick="onAnimationClick(this)">beat fade</button>
+        <button type="button" data-animation="flip" onclick="onAnimationClick(this)">flip</button>
+        <button type="button" data-animation="shake" onclick="onAnimationClick(this)">shake</button>
+        <button type="button" data-animation="spin" onclick="onAnimationClick(this)">spin</button>
+        <button type="button" data-animation="spin-reverse" onclick="onAnimationClick(this)">spin reverse</button>
+        <button type="button" data-animation="pulse" onclick="onAnimationClick(this)">pulse</button>
+        <button type="button" data-animation="spin-pulse" onclick="onAnimationClick(this)">spin pulse</button>
+      </div>
+    </div>
+  </div>
+</dialog>
+`;
+
 export class TransformToHtml extends Transform {
 
   private readonly _fontName: string;
@@ -218,11 +510,12 @@ export class TransformToHtml extends Transform {
 
     output += '<div class="buttons-container">\n';
 
+    output += DIALOG;
+
     return output;
   }
 
   _transform(chunk: BufferWithMeta<SymbolMeta>, _encoding: BufferEncoding, callback: TransformCallback) {
-    const size = Math.max(chunk.metadata.width, chunk.metadata.height);
     let output = '';
     if (!this._isHeaderRendered) {
       output += this._header();
@@ -230,13 +523,20 @@ export class TransformToHtml extends Transform {
     }
 
     output += `
-<button type="button" class="preview" data-text="${this._prefix} ${this._prefix}-${chunk.metadata.name}" aria-label="Copy to clipboard" onclick="onClickCallback(this)">
+<button 
+  type="button" 
+  class="preview" 
+  data-name=${chunk.metadata.name} 
+  data-prefix=${this._prefix} 
+  data-class="${this._prefix}-${chunk.metadata.name}" 
+  onclick="onChooseIcon(this)"
+>
   <span class="inner">
     <i class="${this._prefix} ${this._prefix}-${chunk.metadata.name}"></i>
   </span>
   <br>
   <span class="label">${chunk.metadata.name}</span>
-</button>`
+</button>`;
 
     callback(null, output);
   }
@@ -245,3 +545,4 @@ export class TransformToHtml extends Transform {
     callback(null, '</div>\n</body>\n</html>');
   }
 }
+
